8c9b04246cecd8fdc480aaa7907944961e5ab1de
ruiwuzhang <ruiwuzhang@clubfactory.com>
2020-03-03 16:36:36 +0800

    Updates

---
+++ precache-manifest.9e111c8ce273ba2859adaeca5a351021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0c1da1beeb7640a9c561d7de732168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248462df1fab2f7e11ab",
+    "url": "/static/css/2.5bcfae4a.chunk.css"
+  },
+  {
+    "revision": "21c8d871aa2b90898698",
+    "url": "/static/css/main.e349823a.chunk.css"
+  },
+  {
+    "revision": "248462df1fab2f7e11ab",
+    "url": "/static/js/2.8aa76770.chunk.js"
+  },
+  {
+    "revision": "21c8d871aa2b90898698",
+    "url": "/static/js/main.ac40aa6a.chunk.js"
+  },
+  {
+    "revision": "f5128e168581597993f6",
+    "url": "/static/js/runtime-main.89333823.js"
+  }
+]);
